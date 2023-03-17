e5604074fed3ea32dc74b02895fe15b8134f4535
Ronal Mendoza <ronalmendozat@outlook.com>
2023-03-16 19:29:18 -0500

    innit

---
+++ technical/src/test/java/com/ronal/technical/TechnicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronal.technical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
